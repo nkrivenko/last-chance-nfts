96ecb4e78714d2e2052ab2900c2fc88f48e9f28d
Nikolai Krivenko <nkrivenko35@gmail.com>
2023-01-14 21:41:57 +0000

    Initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,17 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-solhint";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
+
+export default config;
---
+++ scripts/deploy.ts
@@ -0,0 +1,10 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  throw Error("Not implemented yet");
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
