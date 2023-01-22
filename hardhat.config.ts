38f66d51e28b5fc9a110bc1578af876c53593645
Nikolai Krivenko <nkrivenko35@gmail.com>
2023-01-22 12:29:58 +0000

    [#1] Add upgrades library

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,9 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-solhint";
+
+import "@openzeppelin/hardhat-upgrades";
 
 const config: HardhatUserConfig = {
   solidity: {
